cbd601817b991667cdb694356b6f9a1b61acb365
fardeen <70657851+AlphaMale6967@users.noreply.github.com>
2022-10-10 16:19:11 +0530

    commited the day1 code

---
+++ problem1.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 2;
+console.log(x + " +", + y + " is " + (x + y));
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(16 ** 0.5); //Square Root
+console.log(27 ** 0.34);// Cube Root
+console.log(256 ** 0.25);//Forth Root
+console.log(3125 ** 0.20);//Fifth Root
+
+let a = 10;
+let b = 20;
+console.log(((a + b) > (b - a) <= (b / a) > (a % b)));
